f71c066ba03a63a7229104d0126f54ff03188b0e
Jadir J. S. Junior <jadirjsjunior@gmail.com>
2022-02-24 18:19:12 -0300

    create initial page and fix postbuild

--- server/dist/src/app.js
+++ server/dist/src/app.js
@@ -8,6 +8,7 @@ const express_1 = __importDefault(require("express"));
 const api_1 = require("./api");
 const cors_1 = __importDefault(require("cors"));
 const morgan_1 = __importDefault(require("morgan"));
+const path_1 = __importDefault(require("path"));
 const docs_config_1 = require("./docs-config");
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const app = (0, express_1.default)();
@@ -19,5 +20,6 @@ app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, morgan_1.default)('dev'));
 app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(docs_config_1.specs));
+app.use('/', express_1.default.static(path_1.default.join(__dirname, '../public'), { redirect: false }));
 app.use(api_1.api);
 //# sourceMappingURL=app.js.map
--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import express, { Application } from 'express'
 import { api } from './api'
 import cors from 'cors'
 import logger from 'morgan'
+import path from 'path'
 import { specs } from './docs-config'
 import swaggerUi from 'swagger-ui-express'
 
@@ -18,6 +19,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(logger('dev'))
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
+
+app.use('/', express.static(path.join(__dirname, '../public'), { redirect: false }))
 
 app.use(api)
 
